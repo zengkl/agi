a396f860e9f2f8b2ab092906cd7e7b9737af0dc9
zucker <zucker@broadinstitute.org>
2011-10-23 03:00:49 +0000

    gradient Descent

--- ml-class/mlclass-ex1/gradientDescent.m
+++ ml-class/mlclass-ex1/gradientDescent.m
@@ -6,7 +6,7 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
-
+% theta_history = zeros(2, num_iters );
 for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
@@ -16,15 +16,15 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
-
+    for j = 1:length(theta)
+      dJ = 0;
+      for i = 1:m
+        dJ = dJ + ( theta'*X(i,:)' - y(i) )*X(i,j) ;
+      end
+      theta(j) = theta(j) - alpha*dJ/m;
+    end
     % ============================================================
-
+    % theta_history( :, iter) = theta;
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
 
