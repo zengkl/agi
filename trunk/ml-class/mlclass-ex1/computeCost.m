d34f302e169c32480cb2392ae7f7a2cfdfe44242
zucker@broadinstitute.org <zucker@broadinstitute.org@73a2feeb-6406-21af-59cd-812e519bd034>
2011-10-23 02:25:38 +0000

    computeCost

--- trunk/ml-class/mlclass-ex1/computeCost.m
+++ trunk/ml-class/mlclass-ex1/computeCost.m
@@ -11,9 +11,14 @@ J = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
-%               You should set J to the cost.
+%          You should set J to the cost.
 
 
+
+for i = 1:m
+  J = J + (theta'*X(i,:)' - y(i))^2;
+end
+J = J/(2*m);
 
 
 
