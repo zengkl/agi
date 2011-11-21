2813854f382a7c3398f7aedcd976cd3d885f3f60
zucker@broadinstitute.org <zucker@broadinstitute.org@73a2feeb-6406-21af-59cd-812e519bd034>
2011-11-21 00:24:27 +0000

    explored set

--- trunk/ai-class/midterm/question2.py
+++ trunk/ai-class/midterm/question2.py
@@ -8,3 +8,9 @@ q2graph = UndirectedGraph(Dict(
         K=Dict(L=10)))
 q2graph.locations = Dict(A=(15,0),B=(11,0),C=(8,0),D=(7,0),E=(6,0),F=(10,0), G=(2,0), H=(3,0),I=(9,0),J=(5,0),K=(20,0),L=(0,0))
 q2 = GraphProblem( 'A', 'L', q2graph )
+
+f = memoize(lambda n: max(getattr(n, 'f', -infinity), n.path_cost + q2.h(n)), 'f')
+frontier = PriorityQueue(min, f )
+explored = {}
+
+
